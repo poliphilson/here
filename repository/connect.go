68cde45f904c6f3216463f0b06fb5709b8c96663
poliphilson <kwkimg3100@gmail.com>
2022-10-21 15:58:18 +0900

    remove repository account

--- repository/connect.go
+++ repository/connect.go
@@ -1,23 +1,23 @@
 package repository
 
 import (
+	"os"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
 
 func Connect() *gorm.DB {
-	/*
-		user := os.Getenv("DBUSER")
-		pass := os.Getenv("DBPASS")
-		host := os.Getenv("DBHOST")
-		port := os.Getenv("DBPORT")
-		dbName := os.Getenv("DBNAME")
 
-		dsn := user + ":" + pass + "@tcp(" + host + ":" + port + ")/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
-	*/
+	user := os.Getenv("DBUSER")
+	pass := os.Getenv("DBPASS")
+	host := os.Getenv("DBHOST")
+	port := os.Getenv("DBPORT")
+	dbName := os.Getenv("DBNAME")
 
-	dsn := "root:rbdhks12@tcp(34.64.219.30:3306)/how_about_here?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := user + ":" + pass + "@tcp(" + host + ":" + port + ")/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
+
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
 	})

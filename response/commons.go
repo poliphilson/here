553a1664bad7d3f73c84b2519c19c4fde2cc1914
poliphilson <kwkimg3100@gmail.com>
2022-10-24 20:27:33 +0900

    add new response

--- response/commons.go
+++ response/commons.go
@@ -13,3 +13,19 @@ func InternalServerError(c *gin.Context, code int) {
 		"message":   "Internal server error.",
 	})
 }
+
+func BadRequset(c *gin.Context, code int) {
+	c.JSON(http.StatusBadRequest, gin.H{
+		"http_code": http.StatusBadRequest,
+		"here_code": code,
+		"message":   "Bad request.",
+	})
+}
+
+func Ok(c *gin.Context, code int) {
+	c.JSON(http.StatusOK, gin.H{
+		"http_code": http.StatusOK,
+		"here_code": code,
+		"message":   "Ok.",
+	})
+}

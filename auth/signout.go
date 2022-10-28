0080d374e8c9424cc10f60dcc7a9aff1ba30db5a
poliphilson <kwkimg3100@gmail.com>
2022-10-28 20:23:15 +0900

    delete refresh token when sign out

--- auth/signout.go
+++ auth/signout.go
@@ -7,6 +7,11 @@ import (
 )
 
 func SignOut(c *gin.Context) {
+	cookie, _ := c.Request.Cookie("refresh_token")
+	rToken := cookie.Value
+
+	DeleteRefreshTokenFromRedis(rToken)
+
 	c.SetCookie("access_token", "", -1, "/", "localhost", false, true)
 	c.SetCookie("refresh_token", "", -1, "/", "localhost", false, true)
 

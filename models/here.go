e6b4f0fc9b3e7d4fcb33e16346253e3213994c62
poliphilson <kwkimg3100@gmail.com>
2022-10-30 23:15:04 +0900

    edit table schema

--- models/here.go
+++ models/here.go
@@ -7,15 +7,14 @@ import (
 )
 
 type Here struct {
-	HID      int       `gorm:"primaryKey;not null;column:hid"`
-	Uid      int       `gorm:"not null"`
-	User     User      `gorm:"foreignKey:Uid"`
-	Date     time.Time `gorm:"not null"`
-	Contents string
-	Image1   []byte
-	Image2   []byte
-	Image3   []byte
-	Delete   bool              `gorm:"default:false;not null"`
-	Location datatype.Location `gorm:"not null"`
-	Version  int               `gorm:"default:0;not null"`
+	HID       int               `gorm:"primaryKey;not null;column:hid"`
+	Uid       int               `gorm:"not null"`
+	User      User              `gorm:"foreignKey:Uid"`
+	CreatedAt time.Time         `gorm:"not null"`
+	Contents  string            `gorm:"not null"`
+	Delete    bool              `gorm:"default:false;not null"`
+	Image     bool              `gorm:"not null"`
+	Video     bool              `gorm:"not null"`
+	Location  datatype.Location `gorm:"not null"`
+	Version   int               `gorm:"default:0;not null"`
 }

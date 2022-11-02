823bc9e9f72affb294c19f3867a418d81ad0a31e
poliphilson <kwkimg3100@gmail.com>
2022-11-02 22:37:08 +0900

    edit profile image type

--- models/user.go
+++ models/user.go
@@ -6,7 +6,7 @@ type User struct {
 	UID          int    `gorm:"primaryKey;not null;column:uid"`
 	Email        string `gorm:"unique;not null"`
 	Password     string `gorm:"not null"`
-	ProfileImage []byte
+	ProfileImage string
 	Verified     bool      `gorm:"default:false;not null"`
 	CreatedAt    time.Time `gorm:"not null"`
 	Version      int       `gorm:"default:0;not null"`

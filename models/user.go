55b8bbdc9d632ea854acb4358dc9a7dd908bc682
poliphilson <kwkimg3100@gmail.com>
2022-11-02 22:38:33 +0900

    add unique

--- models/user.go
+++ models/user.go
@@ -3,10 +3,10 @@ package models
 import "time"
 
 type User struct {
-	UID          int    `gorm:"primaryKey;not null;column:uid"`
-	Email        string `gorm:"unique;not null"`
-	Password     string `gorm:"not null"`
-	ProfileImage string
+	UID          int       `gorm:"primaryKey;not null;column:uid"`
+	Email        string    `gorm:"unique;not null"`
+	Password     string    `gorm:"not null"`
+	ProfileImage string    `gorm:"unique"`
 	Verified     bool      `gorm:"default:false;not null"`
 	CreatedAt    time.Time `gorm:"not null"`
 	Version      int       `gorm:"default:0;not null"`

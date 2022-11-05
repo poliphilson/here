ab7378726469ac3060b8ac68faf81bdd2ff4f0bc
poliphilson <kwkimg3100@gmail.com>
2022-11-06 00:53:14 +0900

    edit query -> transaction

--- here/detail.go
+++ here/detail.go
@@ -2,16 +2,25 @@ package here
 
 import (
 	"log"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/poliphilson/here/models"
 	"github.com/poliphilson/here/repository"
 	"github.com/poliphilson/here/response"
 	"github.com/poliphilson/here/status"
+	"gorm.io/gorm"
 )
 
 func Detail(c *gin.Context) {
-	hid := c.Param("hid")
+	temp := c.Param("hid")
+	hid, err := strconv.Atoi(temp)
+	if err != nil {
+		response.InternalServerError(c, status.InternalError)
+		log.Println(err.Error())
+		return
+	}
+
 	uid, exists := c.Get("uid")
 	if !exists {
 		response.InternalServerError(c, status.FailedSignIn)
@@ -21,29 +30,31 @@ func Detail(c *gin.Context) {
 	var detailHere response.DetailHere
 
 	mysqlClient := repository.Mysql()
-	err := mysqlClient.Model(&models.Here{}).Where("uid = ? AND hid = ? AND is_deleted = ?", uid, hid, false).Scan(&detailHere.Here).Error
+	err = mysqlClient.Transaction(func(tx *gorm.DB) error {
+		err := tx.Model(&models.Here{}).Where("uid = ? AND hid = ? AND is_deleted = ?", uid, hid, false).Scan(&detailHere.Here).Error
+		if err != nil {
+			return err
+		}
+
+		if detailHere.Here.Image {
+			err := tx.Model(&models.HereImage{}).Select("image").Where("hid = ?", hid).Scan(&detailHere.Images).Error
+			if err != nil {
+				return err
+			}
+		}
+
+		if detailHere.Here.Video {
+			err := tx.Model(&models.HereVideo{}).Select("video").Where("hid = ?", hid).Scan(&detailHere.Videos).Error
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
 	if err != nil {
-		response.InternalServerError(c, status.InternalError)
 		log.Println(err.Error())
+		response.InternalServerError(c, status.InternalError)
 		return
-	}
-
-	if detailHere.Here.Image {
-		err := mysqlClient.Model(&models.HereImage{}).Select("image").Where("hid = ?", hid).Scan(&detailHere.Images).Error
-		if err != nil {
-			response.InternalServerError(c, status.InternalError)
-			log.Println(err)
-			return
-		}
-	}
-
-	if detailHere.Here.Video {
-		err := mysqlClient.Model(&models.HereVideo{}).Select("video").Where("hid = ?", hid).Scan(&detailHere.Videos).Error
-		if err != nil {
-			response.InternalServerError(c, status.InternalError)
-			log.Println(err)
-			return
-		}
 	}
 
 	response.DetailHereOnMap(c, detailHere, status.StatusOK)

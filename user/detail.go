9877cf06dc07359d64704140f484ecf2a210289f
poliphilson <kwkimg3100@gmail.com>
2022-11-27 22:41:00 +0900

    remove email and profile image at context

--- user/detail.go
+++ user/detail.go
@@ -2,26 +2,27 @@ package user
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/poliphilson/here/models"
+	"github.com/poliphilson/here/repository"
 	"github.com/poliphilson/here/response"
 	"github.com/poliphilson/here/status"
 )
 
 func Detail(c *gin.Context) {
-	email, exists := c.Get("email")
-	if !exists {
-		response.InternalServerError(c, status.FailedSignIn)
-		return
-	}
-
-	profileImage, exists := c.Get("profile_image")
+	uid, exists := c.Get("uid")
 	if !exists {
 		response.InternalServerError(c, status.FailedSignIn)
 		return
 	}
 
 	var user response.User
-	user.Email = email.(string)
-	user.ProfileImage = profileImage.(string)
+
+	mysqlClient := repository.Mysql()
+	err := mysqlClient.Model(&models.User{}).Where("uid = ?", uid).Scan(&user).Error
+	if err != nil {
+		response.InternalServerError(c, status.InternalError)
+		return
+	}
 
 	response.UserInformation(c, user, status.StatusOK)
 }
--- user/edit.go
+++ user/edit.go
@@ -6,7 +6,6 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
-	"github.com/poliphilson/here/auth"
 	"github.com/poliphilson/here/here"
 	"github.com/poliphilson/here/models"
 	"github.com/poliphilson/here/repository"
@@ -22,12 +21,6 @@ func Edit(c *gin.Context) {
 	imageBase := os.Getenv("HERE_IMAGE_PATH")
 
 	uid, exists := c.Get("uid")
-	if !exists {
-		response.InternalServerError(c, status.FailedSignIn)
-		return
-	}
-
-	email, exists := c.Get("email")
 	if !exists {
 		response.InternalServerError(c, status.FailedSignIn)
 		return
@@ -49,24 +42,15 @@ func Edit(c *gin.Context) {
 		return
 	}
 
+	var user response.EditUser
+
 	mysqlClient := repository.Mysql()
-
-	err = mysqlClient.Model(&models.User{}).Where("uid = ?", uid).Update("profile_image", profileImage).Error
+	err = mysqlClient.Model(&models.User{}).Where("uid = ?", uid).Update("profile_image", profileImage).Scan(&user).Error
 	if err != nil {
 		response.InternalServerError(c, status.InternalError)
 		log.Println(err.Error())
 		return
 	}
 
-	aToken, err := auth.CreateAccessToken(uid.(int), email.(string), profileImage)
-	if err != nil {
-		response.InternalServerError(c, status.InternalError)
-		log.Println("Fail to create access token.")
-		log.Println(err.Error())
-		return
-	}
-
-	c.SetCookie("access_token", aToken, 60*60*72, "/", "localhost", false, true)
-
-	response.Ok(c, status.StatusOK)
+	response.EditUserInformation(c, user, status.StatusOK)
 }
